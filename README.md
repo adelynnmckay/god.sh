ae59279eccfd1cbcee8dc07081ce59805c79cbfd
adelynnmckay <192640919+adelynnmckay@users.noreply.github.com>
2025-05-20 15:19:26 -0700

    add github workflow, update readme, add god.sh script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # god.sh
+
+> *In the beginning God created the heaven and the earth...*
+
+ade's [god object](https://en.wikipedia.org/wiki/God_object). 
+
+---
+
+have fun.
---
+++ god.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -oue
+
+let_there_be_light() {
+    echo "hello world"
+}
+
+help() {
+cat << EOF
+    
+    In the beginning, God created the 
+    heaven and the earth...
+    
+    Usage: 
+        
+        god.sh let-there-be-light
+
+EOF
+    exit 1
+}
+
+main() {
+    case "$1" in
+    'let-there-be-light')
+        let_there_be_light
+    ;;
+    *)
+        help
+    ;;
+    esac
+}
